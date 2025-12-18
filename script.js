2c24e9e4ea7c420fc954af27abd394ab64ffae6b
Ikramul-hq <59186564+Ikramul-hq@users.noreply.github.com>
2025-12-18 19:23:45 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Mobile Menu Toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+if (menuToggle) {
+    menuToggle.addEventListener('click', () => {
+        sidebar.classList.toggle('active');
+        const icon = menuToggle.querySelector('i');
+        if (sidebar.classList.contains('active')) {
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-times');
+        } else {
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        }
+    });
+}
